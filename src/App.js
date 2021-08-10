28e2b475a78e6b8bf024e940fa1a92351dbce218
Teemu Sormunen <teemu.t.sormunen@gmail.com>
2021-08-10 19:43:59 +0300

    Refactored App.js to hooks.

--- src/App.js
+++ src/App.js
@@ -10,52 +10,48 @@ import Contact from './Components/Contact';
 import Awards from './Components/Awards';
 import Portfolio from './Components/Portfolio';
 
-class App extends Component {
+import { useState, useEffect } from 'react'
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
+const App = () => {
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
+  const [resumeData, setResumeData] = useState({})
 
-  }
+  useEffect(() => {
+    initializeGA()
+    populateResumeData(setResumeData)
+  })
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-  render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Awards data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Header data={resumeData.main}/>
+        <About data={resumeData.main}/>
+        <Resume data={resumeData.resume}/>
+        <Portfolio data={resumeData.portfolio}/>
+        <Awards data={resumeData.testimonials}/>
+        <Contact data={resumeData.main}/>
+        <Footer data={resumeData.main}/>
       </div>
-    );
-  }
+    )
+}
+
+function populateResumeData(setResumeData) {
+  $.ajax({
+    url:'/resumeData.json',
+    dataType:'json',
+    cache: false,
+    success: (data) => {
+      setResumeData(data)
+    },
+    error: function(xhr, status, err){
+      console.log(err);
+      alert(err);
+    }
+  })
+}
+
+function initializeGA() {
+  ReactGA.initialize('UA-110570651-1')
+  ReactGA.pageview(window.location.pathname)
 }
 
 export default App;
